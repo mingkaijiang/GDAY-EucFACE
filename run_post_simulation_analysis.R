182c1d767cee4a72b5ba37c6f252608e6a6c4fbc
mingkaijiang <shanyujordan512@gmail.com>
2020-08-03 16:53:58 +1000

    revised

---
+++ run_post_simulation_analysis.R
@@ -0,0 +1,21 @@
+### THis is the master script to run post-simulation analysis
+### for GDAY simulations
+### Mingkai Jiang (m.jiang@westernsydney.edu.au)
+
+#### call functions
+#### clear wk space
+rm(list=ls(all=TRUE))
+
+#### Source functions and packages
+source("analysis/prepare.R")
+
+
+### Plot met data
+
+### Plot carbon
+
+### Plot nitrogen
+
+### Plot phosphorus
+
+### Plot water
